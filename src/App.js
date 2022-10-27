064f288f5403c96e9c08ef4188857a9f3cc34031
Paola Montiel <112827007+paolamontiel@users.noreply.github.com>
2022-10-27 16:35:06 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './static/logo-ades.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo}  alt="logo" />
+        <p>
+          Sitio en construccion 
+        </p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
